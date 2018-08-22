0cc577e5b68e4188dc11bfa81636579991bfe64c
Mike <mbnator@gmx.net>
2018-08-22 12:06:43 +0200

    wip

--- _testing/post/app.py
+++ _testing/post/app.py
@@ -7,9 +7,20 @@
 ## https://stackoverflow.com/questions/47679398/file-upload-error-in-flask
 
 
-from flask import Flask, request  # import main Flask class and request object
+from flask import Flask, request, jsonify  # import main Flask class and request object
+
+import csv
 
 app = Flask(__name__)  # create the Flask app
+
+def monitor_results(func):
+    def wrapper(*func_args, **func_kwargs):
+        print('function call ' + func.__name__ + '()')
+        retval = func(*func_args,**func_kwargs)
+        print('function ' + func.__name__ + '() returns ' + repr(retval))
+        return retval
+    wrapper.__name__ = func.__name__
+    return wrapper
 
 
 #@app.route('/query-example')
@@ -21,11 +32,26 @@ def query_example():
 # allow both GET and POST requests
 #@app.route('/form-example', methods=['GET', 'POST'])
 @app.route('/fe', methods=['GET', 'POST'])
+@monitor_results
 def form_example():
     print("fe called")
+    #print(request)
     if request.method == 'POST':  # this block is only entered when the form is submitted
+        f = request.form
+        #print(f)
+
+        file = open('outfile.txt', 'w') 
+        writer = csv.writer(file, delimiter = '\t')
+
+        for key in f.keys():
+            for value in f.getlist(key):
+                print(key,":",value)
+                writer.writerow([key] +[":"]+ [value])
+                writer.writerow(["new"] +[":"]+ ["line"])
+                #print(key)
+
+        framework = request.form.get('framework')
         language = request.form.get('language')
-        framework = request.form['framework']
 
         return '''<h1>The language value is: {}</h1>
                   <h1>The framework value is: {}</h1>'''.format(language, framework)
